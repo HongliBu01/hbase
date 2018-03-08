9770150d4e59b013b9bd8d02d51f883ea2f3ce2c
Hongli Bu <honglibu@cs.uchicago.edu>
2018-03-07 19:07:05 -0600

    clean the project

--- target/maketar.sh
+++
@@ -1,9 +0,0 @@
-which cygpath 2> /dev/null
-					if [ $? = 1 ]; then
-						BUILD_DIR="/proj/ucare/honglibu/hbase-0.94.12/target"
-					else
-						BUILD_DIR=`cygpath --unix '/proj/ucare/honglibu/hbase-0.94.12/target'`
-					fi
-
-					cd $BUILD_DIR/hbase-0.94.12
-					tar czf $BUILD_DIR/hbase-0.94.12.tar.gz hbase-0.94.12
